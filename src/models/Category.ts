2d948f06e0d3a6f840e5f397e55c08933cb77d63
Caio Moura <caiomoura1994@gmail.com>
2022-11-14 23:53:36 -0300

    Handle with models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,21 @@
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import Transaction from "./Transaction";
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
+  
+  // @OneToMany(() => Transaction)
+  // transactions: Transaction[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
